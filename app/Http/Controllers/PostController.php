45e7e8f3956ade2a43b113fe08f5f9ef9459f007
Angel Eduardo Ramos Mena <angedramos@gmail.com>
2022-04-07 11:24:55 -0500

    Uploading Changes

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+
+class PostController extends Controller
+{
+    public function list()
+    {
+        $data = [];
+        $data["title"] = "Posts";
+        $data["description"] = "List of posts";
+        $data["posts"] = Post::all();
+        return view('post.list')->with("data", $data);
+    }
+
+    public function show($id)
+    {
+        $data = [];
+        $post = Post::findOrFail($id);
+        $data["title"] = $post->getTitle();
+        $data["description"] = $post->getDescription();
+        $data["post"] = $post;
+        return view('post.show')->with("data", $data);
+    }
+
+    public function save(Request $request)
+    {
+        Post::validate($request);
+        Post::create($request->only(["title","description"]));
+        return back();
+    }
+
+    public function saveComment(Request $request)
+    {
+        Comment::validate($request);
+        Comment::create($request->only(["post_id","message"]));
+        return back();
+    }
+
+    public function deleteComment($id)
+    {
+        $comment = Comment::findOrFail($id);
+        $comment->delete();
+        return back();
+    }
+}
